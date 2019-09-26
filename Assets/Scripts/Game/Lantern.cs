678e9b35c2e39db438d31ba9907429311ac5d5c5
Valefor <emeline.berenguier@gmail.com>
2019-09-26 17:08:46 +0200

    [ADD] Lanterns

--- Assets/Scripts/Game/Lantern.cs
+++ Assets/Scripts/Game/Lantern.cs
@@ -4,20 +4,32 @@ using UnityEngine;
 
 public class Lantern : MonoBehaviour
 {
-    int _angle = 5;
+    int MIN_SPEED = 1;
+    int MAX_SPEED = 5;
+
+    int MIN_ANGLE = 2;
+    int MAX_ANGLE = 5;
+
+    int _angle = 20;
+    int _speed = 10;
+
+    Quaternion _originalRotation;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        _originalRotation = transform.rotation;
+
+        _angle = Random.Range(MIN_ANGLE, MAX_ANGLE);
+        _speed = Random.Range(MIN_SPEED, MAX_SPEED);
     }
 
     // Update is called once per frame
     void Update()
     {
-        Quaternion q = Quaternion.AngleAxis(_angle, Vector3.forward);
-        transform.rotation = Quaternion.Slerp(transform.rotation, q, Time.deltaTime * 15);
+        if (Quaternion.Angle(_originalRotation, transform.rotation) >= _angle) _speed *= -1;
 
-        if (transform.rotation.z == _angle) _angle *= -1;
+        transform.Rotate(Vector3.forward * (_speed * Time.deltaTime));
+        transform.Translate(Vector3.right * _speed  * 0.1f * Time.deltaTime);
     }
 }
