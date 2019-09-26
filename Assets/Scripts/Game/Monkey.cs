c13a700599776f0065bcd95e81cf7f6cd8d40f4e
Valefor <emeline.berenguier@gmail.com>
2019-09-26 11:52:34 +0200

    [ADD] Assets integration

--- Assets/Scripts/Game/Monkey.cs
+++ Assets/Scripts/Game/Monkey.cs
@@ -5,22 +5,93 @@ using UnityEngine;
 [RequireComponent(typeof(SpriteRenderer))]
 public class Monkey : MonoBehaviour
 {
-    SpriteRenderer _sr;
+    //SpriteRenderer _sr;
+
+    [SerializeField] SpriteRenderer _leftHand;
+    [SerializeField] SpriteRenderer _rightHand;
+    [SerializeField] SpriteRenderer _expression;
+
+    float _rightHandTimer = 0;
+    float _leftHandTimer = 0;
+    Enums.TYPE_NOTE _currentState = Enums.TYPE_NOTE.NONE;
+
+    const float TIMER_LEFT = 0.15f;
+    const float TIMER_RIGHT = 0.15f;
+
+    const int LAYER_BACK = 3;
+    const int LAYER_FRONT = 5;
 
     // Start is called before the first frame update
     void Start()
     {
-        _sr = GetComponent<SpriteRenderer>();
-        print(AssetsManager.monkeyNoHandSprite);
-        _sr.sprite = AssetsManager.monkeyNoHandSprite;
-
         EventsManager.Instance.AddListener<OnTap>(UpdatePose);
     }
 
     void UpdatePose(OnTap e)
     {
-        if (e.isRight) _sr.sprite = AssetsManager.monkeyRightHandSprite;
-        else _sr.sprite = AssetsManager.monkeyLeftHandSprite;
+        if (GameManager.instance.state != Enums.GAME_STATE.GAME) return;
+
+        if (e.isRight)
+        {
+            SetHandMode(Enums.TYPE_NOTE.RIGHT);
+            _rightHandTimer = TIMER_RIGHT;
+        }
+
+        else
+        {
+            SetHandMode(Enums.TYPE_NOTE.LEFT);
+            _leftHandTimer = TIMER_LEFT;
+        }
+    }
+
+    private void Update()
+    {
+        if (_leftHandTimer < 0) _leftHandTimer = 0;
+        if (_rightHandTimer < 0) _rightHandTimer = 0;
+
+        if (_leftHandTimer > 0) _leftHandTimer -= Time.deltaTime;
+        if (_rightHandTimer > 0) _rightHandTimer -= Time.deltaTime;
+
+        if (_leftHandTimer <= 0 && _rightHandTimer <= 0 && _currentState != Enums.TYPE_NOTE.NONE)
+        {
+            SetHandMode(Enums.TYPE_NOTE.NONE);
+
+            _leftHandTimer = 0;
+            _rightHandTimer = 0;
+        }
+
+    }
+
+    void SetHandMode(Enums.TYPE_NOTE pWantedState)
+    {
+        _currentState = pWantedState;
+
+        switch (_currentState)
+        {
+            case Enums.TYPE_NOTE.RIGHT:
+                _rightHand.sprite = AssetsManager.monkeyRightHandDownSprite;
+                _rightHand.sortingOrder = LAYER_FRONT;
+
+                _leftHand.sprite = AssetsManager.monkeyLeftHandUpSprite;
+                _leftHand.sortingOrder = LAYER_BACK;
+                break;
+
+            case Enums.TYPE_NOTE.NONE:
+                _rightHand.sprite = AssetsManager.monkeyRightHandUpSprite;
+                _rightHand.sortingOrder = LAYER_BACK;
+
+                _leftHand.sprite = AssetsManager.monkeyLeftHandUpSprite;
+                _leftHand.sortingOrder = LAYER_BACK;
+                break;
+
+            case Enums.TYPE_NOTE.LEFT:
+                _leftHand.sprite = AssetsManager.monkeyLeftHandDownSprite;
+                _leftHand.sortingOrder = LAYER_FRONT;
+
+                _rightHand.sprite = AssetsManager.monkeyRightHandUpSprite;
+                _rightHand.sortingOrder = LAYER_BACK;
+                break;
+        }
     }
 
     private void OnDestroy()
---
+++ Assets/Scripts/Utils/Blink.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Blink : MonoBehaviour
+{
+    Text _text;
+
+    private void Start()
+    {
+        _text = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Color _alpha = _text.color;
+        _alpha.a = 0.3f + 0.7f * Mathf.Sin(Time.frameCount * 0.05f);
+        _text.color = _alpha;
+    }
+}
